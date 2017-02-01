dce81f00125d4fb6b6f5c421bfb35a295528e0cf
Marcio Augusto Diniz <dnz.marcio@gmail.com>
2017-02-01 11:09:50 -0800

    Adding new plots.

--- R/plot.R
+++ R/plot.R
@@ -5,7 +5,8 @@ plot.ewoc_d1basic <- function(x, ...){
   sm <- summary(object, print = FALSE)
 
   mtd <- as.numeric(object$mtd)
-  data_plot <- data.frame(x = mtd)
+  data_plot <- data.frame(mtd)
+
   dens <- density(mtd)
   shade <- with(dens, data.frame(x, y))
 
@@ -15,7 +16,7 @@ plot.ewoc_d1basic <- function(x, ...){
     geom_vline(xintercept = as.numeric(sm$hpd_dose),
                linetype = 2, size = 1.2) +
     geom_ribbon(data =
-                subset(shade, x > sm$hpd_dose[1] & x < sm$next_dose),
+                subset(shade, x > sm$hpd_dose[1] & x < sm$hpd_dose[2]),
                 aes(ymax = y, x = x), ymin = 0, fill="red", alpha=0.3) +
     labs(y = "Density", x = "MTD") +
     annotate("text", x = sm$next_dose + 0.10*sm$next_dose,
@@ -26,3 +27,65 @@ plot.ewoc_d1basic <- function(x, ...){
   plot(gp)
   return(gp)
 }
+
+#'@export
+plot.ewoc_d1extended <- function(x, ...){
+
+  object <- x
+  sm <- summary(object, print = FALSE)
+
+  mtd <- as.numeric(object$mtd)
+  data_plot <- data.frame(x = mtd)
+
+  dens <- density(mtd, n = 2^15)
+  shade <- with(dens, data.frame(x, y))
+
+  label <- paste("Next dose:", round(sm$next_dose, 2))
+
+  gp <- ggplot(data_plot, aes(x = mtd)) + geom_density() +
+    geom_vline(xintercept = as.numeric(sm$hpd_dose),
+               linetype = 2, size = 1.2) +
+    geom_ribbon(data =
+                subset(shade, x > max(sm$hpd_dose[1], object$trial$min_dose()) &
+                         x < min(sm$hpd_dose[2], object$trial$max_dose())),
+                aes(ymax = y, x = x), ymin = 0, fill = "red", alpha = 0.3) +
+    labs(y = "Density", x = "MTD") +
+    annotate("text", x = sm$next_dose + 0.30*sm$next_dose,
+             y = max(shade$y)/2,
+             label = label) +
+    theme_bw()
+
+  plot(gp)
+  return(gp)
+}
+
+#'@export
+plot.ewoc_d1ph <- function(x, ...){
+
+  object <- x
+  sm <- summary(object, print = FALSE)
+
+  mtd <- as.numeric(object$mtd)
+  data_plot <- data.frame(mtd)
+
+  dens <- density(mtd)
+  shade <- with(dens, data.frame(x, y))
+
+  label <- paste("Next dose:", round(sm$next_dose, 2))
+
+  gp <- ggplot(data_plot, aes(x = mtd)) + geom_density() +
+    geom_vline(xintercept = as.numeric(sm$hpd_dose),
+               linetype = 2, size = 1.2) +
+    geom_ribbon(data =
+                  subset(shade, x > sm$hpd_dose[1] & x < sm$hpd_dose[2]),
+                aes(ymax = y, x = x), ymin = 0, fill="red", alpha=0.3) +
+    labs(y = "Density", x = "MTD") +
+    annotate("text", x = sm$next_dose + 0.10*sm$next_dose,
+             y = max(shade$y)/2,
+             label = label) +
+    theme_bw()
+
+  plot(gp)
+  return(gp)
+}
+
