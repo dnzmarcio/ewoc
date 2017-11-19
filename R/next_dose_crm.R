1bc62ce0fe72e75a67f4971c9454627f6d9700e9
dnz.marcio@gmail.com <dnz.marcio@gmail.com>
2017-11-19 00:17:53 -0800

    Add CRM.

---
+++ R/next_dose_crm.R
@@ -0,0 +1,34 @@
+next_dose.crm_d1classic <- function(data){
+
+  beta <- data$mcmc$beta
+  beta_est <- apply(beta, 2, median)
+
+  crm_criterion <- function(dose){
+    design_matrix <- cbind(1, dose)
+    lp <- design_matrix %*% beta_est
+    out <- abs(as.numeric(plogis(lp)) - data$theta)
+    return(out)
+  }
+
+  next_gamma <- optimize(f = crm_criterion,
+                        lower = 0,
+                        upper = 1)$minimum
+  next_dose <- inv_standard_dose(dose = next_gamma,
+                                 min_dose = data$limits$min_dose,
+                                 max_dose = data$limits$max_dose)
+  next_dose <- ifelse(next_dose > data$limits$last_dose,
+                      data$limits$last_dose,
+                      ifelse(next_dose < data$limits$first_dose,
+                             data$limits$first_dose, next_dose))
+  next_gamma <- standard_dose(dose = next_dose,
+                              min_dose = data$limits$min_dose,
+                              max_dose = data$limits$max_dose)
+
+  pdlt <- as.numeric(plogis(cbind(1, next_gamma)%*%t(beta)))
+
+    out <- list(mtd = data$mcmc$mtd, pdlt = pdlt, next_dose = next_dose,
+                rho = data$mcmc$rho, gamma = data$mcmc$gamma,
+                sample = data$mcmc$sample)
+  return(out)
+}
+
