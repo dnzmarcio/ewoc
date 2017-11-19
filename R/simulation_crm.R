65dca59c6ca60246e3112688248f5fd82ad8d7be
Marcio Augusto Diniz <dnz.marcio@gmail.com>
2017-11-19 09:34:29 -0800

    Fix examples.

--- R/simulation_crm.R
+++ R/simulation_crm.R
@@ -88,7 +88,7 @@
 #'}
 #'
 #'\dontrun{
-#'### Classic EWOC
+#'### Classic CRM
 #'DLT <- 0
 #'dose <- 30
 #'step_zero <- crm_d1classic(DLT ~ dose, type = 'discrete',
@@ -105,7 +105,7 @@
 #'                      response_sim = response_sim,
 #'                      ncores = 2)
 #'
-#'### Extended EWOC
+#'### Extended CRM
 #'DLT <- 0
 #'dose <- 30
 #'step_zero <- crm_d1extended(DLT ~ dose, type = 'discrete',
@@ -121,7 +121,7 @@
 #'                      response_sim = response_sim,
 #'                      ncores = 2)
 #'
-#'### PH EWOC
+#'### PH CRM
 #'time <- 0
 #'status <- 0
 #'dose <- 30
