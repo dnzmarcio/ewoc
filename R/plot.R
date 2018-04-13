62fe1405dadeb1e5cf4951ffc6d9500b1624b79a
dnz.marcio@gmail.com <dnz.marcio@gmail.com>
2018-04-13 10:54:29 -0700

    Add EWOC POS.

--- R/plot.R
+++ R/plot.R
@@ -90,3 +90,33 @@ plot.d1ph <- function(x, ...){
   return(out)
 }
 
+#'@import ggplot2 graphics
+#'@export
+plot.d1pos <- function(x, ...){
+
+  object <- x
+  sm <- summary(object, print = FALSE)
+
+  mtd <- as.numeric(object$mtd)
+  data_plot <- data.frame(mtd)
+
+  dens <- density(mtd)
+  shade <- data.frame(x = dens$x, y = dens$y)
+
+  label <- paste("Next dose:", round(sm$next_dose, 2))
+
+  out <- ggplot(data_plot, aes_string(x = 'mtd')) + geom_density() +
+    geom_vline(xintercept = as.numeric(sm$next_dose),
+               linetype = 2, size = 1.2) +
+    geom_ribbon(data = shade[shade$x > sm$hpd_dose[1] &
+                               shade$x < sm$hpd_dose[2], ],
+                aes_string(ymax = 'y', x = 'x'), ymin = 0, fill="red", alpha=0.3) +
+    labs(y = "Density", x = "MTD") +
+    annotate("text", x = sm$next_dose,
+             y = max(shade$y)/2, hjust = -0.20,
+             label = label) +
+    theme_bw()
+
+  return(out)
+}
+
