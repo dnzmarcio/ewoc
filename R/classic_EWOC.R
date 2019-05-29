1a798db7767956985a853ad132eedbe9f2d1e5c0
Diniz <Marcio.Diniz@cshs.org>
2019-05-29 16:00:33 -0700

    Fix examples.

--- R/classic_EWOC.R
+++ R/classic_EWOC.R
@@ -57,8 +57,8 @@
 #'dose <- 20
 #'test <- ewoc_d1classic(DLT ~ dose, type = 'discrete',
 #'                       theta = 0.33, alpha = 0.25,
-#'                       min_dose = 0, max_dose = 100,
-#'                       dose_set = seq(0, 100, 20),
+#'                       min_dose = 20, max_dose = 100,
+#'                       dose_set = seq(20, 100, 20),
 #'                       rho_prior = matrix(1, ncol = 2, nrow = 1),
 #'                       mtd_prior = matrix(1, ncol = 2, nrow = 1),
 #'                       rounding = "nearest")
--- R/simulation_ewoc.R
+++ R/simulation_ewoc.R
@@ -48,16 +48,16 @@
 #'\dontshow{
 #'### Classic EWOC
 #'DLT <- 0
-#'dose <- 30
+#'dose <- 20
 #'step_zero <- ewoc_d1classic(DLT ~ dose, type = 'discrete',
 #'                            theta = 0.33, alpha = 0.25,
-#'                            min_dose = 0, max_dose = 100,
-#'                            dose_set = seq(0, 100, 20),
+#'                            min_dose = 20, max_dose = 100,
+#'                            dose_set = seq(20, 100, 20),
 #'                            rho_prior = matrix(1, ncol = 2, nrow = 1),
 #'                            mtd_prior = matrix(1, ncol = 2, nrow = 1),
 #'                            rounding = "nearest")
-#'response_sim <- response_d1classic(rho = 0.05, mtd = 20, theta = 0.33,
-#'                                   min_dose = 10, max_dose = 50)
+#'response_sim <- response_d1classic(rho = 0.05, mtd = 60, theta = 0.33,
+#'                                   min_dose = 20, max_dose = 100)
 #'sim <- ewoc_simulation(step_zero = step_zero,
 #'                       n_sim = 1, sample_size = 30, n_cohort = 2,
 #'                       alpha_strategy = "increasing",
@@ -66,15 +66,15 @@
 #'
 #'### Extended EWOC
 #'DLT <- 0
-#'dose <- 30
+#'dose <- 20
 #'step_zero <- ewoc_d1extended(DLT ~ dose, type = 'discrete',
 #'                            theta = 0.33, alpha = 0.25,
-#'                            min_dose = 0, max_dose = 100,
-#'                            dose_set = seq(0, 100, 20),
+#'                            min_dose = 20, max_dose = 100,
+#'                            dose_set = seq(20, 100, 20),
 #'                            rho_prior = matrix(1, ncol = 2, nrow = 2),
 #'                            rounding = "nearest")
 #'response_sim <- response_d1extended(rho = c(0.05, 0.5),
-#'                                    min_dose = 10, max_dose = 50)
+#'                                    min_dose = 20, max_dose = 100)
 #'sim <- ewoc_simulation(step_zero = step_zero,
 #'                       n_sim = 1, sample_size = 2,
 #'                       alpha_strategy = "increasing",
@@ -84,18 +84,18 @@
 #'### PH EWOC
 #'time <- 0
 #'status <- 0
-#'dose <- 30
+#'dose <- 20
 #'
 #'step_zero <- ewoc_d1ph(cbind(time, status) ~ dose, type = 'discrete',
 #'                       theta = 0.33, alpha = 0.25, tau = 10,
-#'                       min_dose = 30, max_dose = 50,
-#'                       dose_set = seq(30, 50, 5),
+#'                       min_dose = 20, max_dose = 100,
+#'                       dose_set = seq(20, 100, 20),
 #'                       rho_prior = matrix(1, ncol = 2, nrow = 1),
 #'                       mtd_prior = matrix(1, ncol = 2, nrow = 1),
 #'                       distribution = 'exponential',
 #'                       rounding = 'nearest')
 #'response_sim <- response_d1ph(rho = 0.05, mtd = 40, theta = 0.33,
-#'                              min_dose = 30, max_dose = 50,
+#'                              min_dose = 20, max_dose = 100,
 #'                              tau = 10, distribution = "exponential")
 #'sim <- ewoc_simulation(step_zero = step_zero,
 #'                       n_sim = 1, sample_size = 2,
@@ -107,16 +107,16 @@
 #'\dontrun{
 #'### Classic EWOC
 #'DLT <- 0
-#'dose <- 30
+#'dose <- 20
 #'step_zero <- ewoc_d1classic(DLT ~ dose, type = 'discrete',
 #'                            theta = 0.33, alpha = 0.25,
-#'                            min_dose = 0, max_dose = 100,
-#'                            dose_set = seq(0, 100, 20),
+#'                            min_dose = 20, max_dose = 100,
+#'                            dose_set = seq(20, 100, 20),
 #'                            rho_prior = matrix(1, ncol = 2, nrow = 1),
 #'                            mtd_prior = matrix(1, ncol = 2, nrow = 1),
 #'                            rounding = "nearest")
-#'response_sim <- response_d1classic(rho = 0.05, mtd = 20, theta = 0.33,
-#'                                   min_dose = 10, max_dose = 50)
+#'response_sim <- response_d1classic(rho = 0.05, mtd = 60, theta = 0.33,
+#'                                   min_dose = 20, max_dose = 100)
 #'sim <- ewoc_simulation(step_zero = step_zero,
 #'                        n_sim = 2, sample_size = 30, n_cohort = 1,
 #'                        alpha_strategy = "increasing",
@@ -125,15 +125,15 @@
 #'
 #'### Extended EWOC
 #'DLT <- 0
-#'dose <- 30
+#'dose <- 20
 #'step_zero <- ewoc_d1extended(DLT ~ dose, type = 'discrete',
 #'                            theta = 0.33, alpha = 0.25,
-#'                            min_dose = 0, max_dose = 100,
-#'                            dose_set = seq(0, 100, 20),
+#'                            min_dose = 20, max_dose = 100,
+#'                            dose_set = seq(20, 100, 20),
 #'                            rho_prior = matrix(1, ncol = 2, nrow = 2),
 #'                            rounding = "nearest")
 #'response_sim <- response_d1extended(rho = c(0.05, 0.5),
-#'                                    min_dose = 10, max_dose = 50)
+#'                                    min_dose = 20, max_dose = 100)
 #'sim <- ewoc_simulation(step_zero = step_zero,
 #'                        n_sim = 2, sample_size = 30,
 #'                        alpha_strategy = "increasing",
@@ -143,18 +143,18 @@
 #'### PH EWOC
 #'time <- 0
 #'status <- 0
-#'dose <- 30
+#'dose <- 20
 #'
 #'step_zero <- ewoc_d1ph(cbind(time, status) ~ dose, type = 'discrete',
 #'                       theta = 0.33, alpha = 0.25, tau = 10,
-#'                       min_dose = 30, max_dose = 50,
-#'                       dose_set = seq(30, 50, 5),
+#'                       min_dose = 20, max_dose = 100,
+#'                       dose_set = seq(20, 100, 20),
 #'                       rho_prior = matrix(1, ncol = 2, nrow = 1),
 #'                       mtd_prior = matrix(1, ncol = 2, nrow = 1),
 #'                       distribution = 'exponential',
 #'                       rounding = 'nearest')
-#'response_sim <- response_d1ph(rho = 0.05, mtd = 40, theta = 0.33,
-#'                              min_dose = 30, max_dose = 50,
+#'response_sim <- response_d1ph(rho = 0.05, mtd = 60, theta = 0.33,
+#'                              min_dose = 20, max_dose = 100,
 #'                              tau = 10, distribution = "exponential")
 #'sim <- ewoc_simulation(step_zero = step_zero,
 #'                        n_sim = 2, sample_size = 30,
